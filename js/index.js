20467dfe970bb6d7c2b16b753fd5163ccc24ddc4
namikah <namikah@code.edu.az>
2022-01-07 16:11:22 +0400

    navbar scroll effect added

--- js/index.js
+++ js/index.js
@@ -7,3 +7,28 @@ $(".drop-menu").click(function (e) {
     e.preventDefault();
     $(this).next(".sub-menu").toggle();
 })
+
+navbarScrollEffect();
+
+function navbarScrollEffect() {
+    $(window).scroll(function (e) {
+        console.log($(this).scrollTop());
+        if ($(this).scrollTop() < 80) {
+            $(".navbar").css("transition", "0.3s all");
+            $(".navbar").removeClass("active-scroll");
+            $(".navbar").css("transform", "translateY(0)");
+            $(".navbar").css("opacity", "1");
+        }
+        else if ($(this).scrollTop() > 80 && $(this).scrollTop() < 100) {
+            $(".navbar").css("transition", "0s");
+            $(".navbar").css("transform", "translateY(-100%)");
+            $(".navbar").css("opacity", "0");
+        }
+        else if ($(this).scrollTop() > 100) {
+            $(".navbar").css("transition", "1s all");
+            $(".navbar").css("transform", "translateY(0)");
+            $(".navbar").addClass("active-scroll");
+            $(".navbar").css("opacity", "1");
+        }
+    });
+}
