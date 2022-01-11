96b0a35d54c069d908df696ac54399e594da72c9
Namik <namikah@code.edu.az>
2022-01-11 20:15:59 +0400

    parallax section auto counter added

--- js/home.js
+++ js/home.js
@@ -14,3 +14,74 @@ $(".latests-blog").click(function(e){
     e.preventDefault();
     window.scrollTo($('#latests-blog'), 4163);
 })
+
+// let number=0;
+
+//  function asdasd(){
+//         $(".project-complate").text(number);
+//         parallaxCount();
+//  }
+
+// function parallaxCount(){
+//     number++;
+//     asdasd();
+// }
+
+// $(".project-complate").each(function(e) {
+//     console.log($(".project-complate").text());
+//     var tmp = $(".project-complate").text();
+//     var i = 0;
+//     while(i != tmp) {
+//         $(".project-complate").text(i++);
+//     }
+// });
+
+    
+    $(".project-complate").each(function() {
+        var that = $(this),
+            tmp = that.html(),
+            i = 0,
+            interval;
+
+        that.html(0);
+        interval = setInterval(function() {
+            that.html(++i);
+            if (i === +tmp) clearInterval(interval);
+        }, 10);
+    });
+    $(".happy-customer").each(function() {
+        var that = $(this),
+            tmp = that.html(),
+            i = 0,
+            interval;
+
+        that.html(0);
+        interval = setInterval(function() {
+            that.html(++i);
+            if (i === +tmp) clearInterval(interval);
+        }, 5);
+    });
+    $(".experienced-context").each(function() {
+        var that = $(this),
+            tmp = that.html(),
+            i = 0,
+            interval;
+
+        that.html(0);
+        interval = setInterval(function() {
+            that.html(++i);
+            if (i === +tmp) clearInterval(interval);
+        }, 79);
+    });
+    $(".ongoing-project").each(function() {
+        var that = $(this),
+            tmp = that.html(),
+            i = 0,
+            interval;
+
+        that.html(0);
+        interval = setInterval(function() {
+            that.html(++i);
+            if (i === +tmp) clearInterval(interval);
+        }, 21);
+    });
